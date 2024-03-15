ec909fd15411684c5cd897f71e460945c429ae31
Edwin Rosales <72425664+el-maistro@users.noreply.github.com>
2024-03-14 23:09:43 -0600

    mod_file_manager
    Tamaño de imagen .png debe ser 32x32

--- src/panel_file_manager.cpp
+++ src/panel_file_manager.cpp
@@ -6,10 +6,29 @@ extern Servidor* p_Servidor;
 extern std::mutex vector_mutex;
 
 panelFileManager::panelFileManager(wxWindow* pParent) :
-	wxPanel(pParent, EnumIDS::ID_Panel_FM, wxDefaultPosition, wxDefaultSize, wxDD_DEFAULT_STYLE) {
+	wxPanel(pParent, EnumIDS::ID_Panel_FM) {
 	this->SetBackgroundColour(wxColor(200, 200, 200));
 	
 	//Crear barra lateral dentro del panel con accesos rapidos como
 	//dispositivos conectados, desktop, downloads, etc...
-	
+	this->p_ToolBar = new wxToolBar(this, wxID_ANY, wxDefaultPosition, wxDefaultSize, wxTB_VERTICAL | wxTB_LEFT);
+	wxImage::AddHandler(new wxPNGHandler);
+
+	wxBitmap openBitmap(wxT("C:\\copy.png"), wxBITMAP_TYPE_PNG);
+	wxBitmap desktopBitmap(wxT("C:\\desktop.png"), wxBITMAP_TYPE_PNG);
+	wxBitmap downloadBitmap(wxT("C:\\download.png"), wxBITMAP_TYPE_PNG);
+
+
+	this->p_ToolBar->AddTool(wxID_OPEN, wxT("Open"), openBitmap, "Test");
+	this->p_ToolBar->AddSeparator(); // Separador entre grupos de botones
+	this->p_ToolBar->AddTool(wxID_SAVE, wxT("Escritorio"), desktopBitmap, "Escritorio");
+	this->p_ToolBar->AddSeparator(); // Separador entre grupos de botones
+	this->p_ToolBar->AddTool(wxID_EXIT, wxT("Descargas"), downloadBitmap, "Descargas");
+	this->p_ToolBar->Realize();
+
+	wxBoxSizer* sizer = new wxBoxSizer(wxHORIZONTAL);
+	sizer->Add(this->p_ToolBar, 0, wxEXPAND);
+	sizer->Add(new wxStaticText(this, wxID_ANY, "Testing"), 0, wxEXPAND);
+	SetSizer(sizer);
+
 }
